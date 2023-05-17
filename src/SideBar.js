40b9dd1672c98d3176e9f24d944ddeda4d498370
Ankit-0040 <ankitssingh1912@gmail.com>
2023-05-18 00:33:39 +0530

    sidebar update

--- src/SideBar.js
+++ src/SideBar.js
@@ -15,7 +15,7 @@ function SideBar() {
         <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="" />
         <Avatar className="sidebar__avatar" />
         <h2>Ankit Singh</h2>
-        <h4>ankitsingh@gmail.com</h4>
+        <h4>ankitssingh1912@gmail.com</h4>
       </div>
       <div className="sidebar__stats">
         <div className="sidebar__stat">
